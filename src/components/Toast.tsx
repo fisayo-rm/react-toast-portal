97beee06878781b7dc98325cead9f11248ae444d
Fisayo <aikomofisayo@gmail.com>
2024-03-27 07:48:37 +0100

    Update Toast component

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
+import { Toast } from "../type";
+import { useToast } from "../ToastContext";
 
-export type ToastProps = {
-  message: string;
-  onClose: () => void;
+type ToastProps = {
+  status: Toast;
+  baseIconClass?: string;
+  delayed?: boolean;
 };
 
-const Toast: React.FC<ToastProps> = ({ message, onClose }) => {
+const Toast: React.FC<ToastProps> = ({ status, delayed }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const { removeToast } = useToast();
+  const notificationClass = useMemo(() => {
+    const classes: Record<string, boolean> = {};
+
+    if (status.mode === "loader") {
+      classes["rtp-cursor-wait"] = true;
+    }
+
+    classes[`rtp-theme-${status.theme}`] = true;
+
+    return Object.keys(classes)
+      .filter((key) => classes[key])
+      .join(" ");
+  }, [status.mode, status.theme]);
+
+  const isNotification = useMemo(
+    () => ["prompt", "loader"].indexOf(status.mode!) === -1,
+    [status.mode],
+  );
+
+  const dismiss = (id: string) => {
+    if (isNotification) removeToast(id);
+  };
   return (
-    <div>
-      {message}
-      <button onClick={onClose}>X</button>
+    <div
+      className={`rtp-notification ${notificationClass}`}
+      role="alert"
+      draggable="false"
+      data-delayed={delayed}
+      onClick={() => dismiss(status.id)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onTouchStart={() => setIsHovered(true)}
+      onTouchEnd={() => setIsHovered(false)}
+    >
+      <div className="rtp-content">
+        {status.title && <h2 className="rtp-title">{status.title}</h2>}
+        {status.body && <h2 className="rtp-paragraph">{status.title}</h2>}
+      </div>
     </div>
   );
 };
